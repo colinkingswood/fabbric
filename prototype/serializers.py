5b20c65d4e3561d3069f3e584d27311efe24c3c5
colin <colinkingswood@gmail.com>
2024-04-11 23:06:29 +0200

    Ok, basicas working, needs tidied up.

--- prototype/serializers.py
+++ prototype/serializers.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
 from .models import ClothingItem
+from sorl.thumbnail import get_thumbnail
 
 class ClothingItemSerializer(serializers.ModelSerializer):
+    thumbnail = serializers.SerializerMethodField()
+
     class Meta:
         model = ClothingItem
-        fields = ['id', 'name', 'image', 'color', 'description']
+        fields = ['id', 'name', 'thumbnail', 'color', 'description']
+
+    def get_thumbnail(self, obj):
+        """
+        This will generate and cache a thumbnail
+        :param obj:
+        :return: a url for a thumbnail image
+        """
+        if obj.image:
+            # Generate a thumbnail of the desired size (e.g., 100x100)
+            thumbnail = get_thumbnail(obj.image, '100x100', crop='center', quality=99)
+            return self.context['request'].build_absolute_uri(thumbnail.url)
+        return None
